0dc82974caf9139689d130effa741fce4ad99809
Oleh <oleg.tisovsky228@gmail.com>
2024-05-01 12:35:04 +0300

    c

--- config/database.js
+++ config/database.js
@@ -1,8 +1,10 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
+const tedious = require("tedious");
 
-const sequelize = new Sequelize('monobank', 'tysyk-monobank', 'A2s4D6f8', {
-  host: 'monobank.database.windows.net',
-  dialect: 'mssql',
+const sequelize = new Sequelize("monobank", "tysyk-monobank", "A2s4D6f8", {
+  host: "monobank.database.windows.net",
+  dialect: "mssql",
+  dialectModule: tedious,
   dialectOptions: {
     options: {
       encrypt: true,
@@ -13,9 +15,9 @@ const sequelize = new Sequelize('monobank', 'tysyk-monobank', 'A2s4D6f8', {
 async function testConnection() {
   try {
     await sequelize.authenticate();
-    console.log('<-Connected to database successfully.->');
+    console.log("<-Connected to database successfully.->");
   } catch (error) {
-    console.error('!-Error while connecting to database:', error,);
+    console.error("!-Error while connecting to database:", error);
   }
 }
 
